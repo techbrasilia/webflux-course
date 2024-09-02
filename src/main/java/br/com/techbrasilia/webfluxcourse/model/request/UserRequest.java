43859be4a905ef1ba912f131f3a1a5164b3bab7f
fabio.carvalho <dbc.fabio@unimedpoa.com.br>
2024-09-02 15:05:33 -0300

    add Validação espaços com trim validator

--- src/main/java/br/com/techbrasilia/webfluxcourse/model/request/UserRequest.java
+++ src/main/java/br/com/techbrasilia/webfluxcourse/model/request/UserRequest.java
@@ -1,16 +1,22 @@
 package br.com.techbrasilia.webfluxcourse.model.request;
 
+import br.com.techbrasilia.webfluxcourse.validators.TrimString;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
 public record UserRequest(
+        @TrimString
         @Size(min = 3, max = 50,message = "must be between 3 and 50 characters")
         @NotBlank(message = "must not be null or empty")
         String name,
+
+        @TrimString
         @Email(message = "invalid email")
         @NotBlank(message = "must not be null or empty")
         String email,
+
+        @TrimString
         @Size(min = 3, max = 50, message = "must be betweem 3 and 50 characters")
         @NotBlank(message = "must not be null or empty")
         String password
